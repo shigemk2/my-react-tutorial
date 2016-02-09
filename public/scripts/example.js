e2fb41ac489612b2585ada8f5a18cd07edaeec14
shigemk2 <i.am.shige@gmail.com>
2016-02-09 23:25:48 +0900

    tutorial1-raw.js

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,13 +1,13 @@
-var CommentBox = React.createClass({
+var CommentBox = React.createClass({displayName: 'CommentBox',
   render: function() {
     return (
-      <div clasName="commentBox">
-        Hello, world! I am a commentBox.
-      </div>
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world! I am a CommentBox."
+      )
     );
   }
 });
 ReactDOM.render(
-  <CommentBox />,
+  React.createElement(CommentBox, null),
   document.getElementById('content')
 );
