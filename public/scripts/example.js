d12686a17b2fc111abdaeedd1cf874b9f3f86aa4
shigemk2 <i.am.shige@gmail.com>
2016-02-14 17:53:53 +0900

    tutorial8.js データモデルとの連携 ソースコードにコメントを直に書き込んでいたのをやめて、JSONのblobデータをコメントリストにレンダリングしたい まずはソースコードの中にデータを書いておく

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,3 +1,8 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
   render: function() {
     return (
